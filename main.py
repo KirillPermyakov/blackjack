46c3cd856de4d2b29ff5b75d4bf77ef6ec5d77cc
Kirill <permyakov.cyrill@yandex.ru>
2022-11-08 19:10:13 +0300

    add new function

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import random
 
 deck_count = 4
 
-types_of_cards = ['K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
+types_of_cards = {'A': 11,
+                  'K': 10,
+                  'Q': 10,
+                  'J': 10,
+                  '10': 10,
+                  '9': 9,
+                  '8': 8,
+                  '7': 7,
+                  '6': 6,
+                  '5': 5,
+                  '4': 4,
+                  '3': 3,
+                  '2': 2}
 
 
 def generate_deck(cards, decks):
@@ -27,5 +39,30 @@ def deal_cards():
 dealer = deal_cards()
 hand = deal_cards()
 
-print(dealer, hand)
-print(deck)
+
+def hand_values(hand, cards):
+    sum_of_values = 0
+    for card in hand:
+        sum_of_values += cards.get(card)
+    return sum_of_values
+
+
+values = hand_values(hand, types_of_cards)
+dealer_values = hand_values(dealer, types_of_cards)
+print(hand)
+print(values)
+#print(deck)
+while not values >= 21:
+    more = input('взять ещё карту? ')
+    if more == 'да':
+        hand.append(deck.pop(0))
+        values = hand_values(hand, types_of_cards)
+        print(hand)
+        print(values)
+    elif more == 'нет':
+        if values > dealer_values and values <= 21:
+            print('you win')
+            break
+        else:
+            print('you lose')
+            break
